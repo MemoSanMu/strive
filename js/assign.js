9e9a1c986118c83369e12dfc195c0a3c74ed069b
王森 <wangsen1@yonghui.cn>
2023-01-30 15:08:27 +0800

    chore(assign): 合并增加注解

--- js/assign.js
+++ js/assign.js
@@ -3,30 +3,34 @@
 export function assign() {
   for (let i = arguments.length - 1; i > 0; i--) {
     for (let key in arguments[i]) {
-      if (key === "__proto__") {
-        continue;
+      if (key === '__proto__') {
+        continue
       }
-      arguments[i - 1][key] = arguments[i][key];
+      arguments[i - 1][key] = arguments[i][key]
     }
   }
 
-  return arguments[0];
+  return arguments[0]
 }
-
 
 /**
  * while循环语句
  * @returns objects combine
  */
 export function assignWhile() {
- let index = arguments.length 
+  let index = arguments.length
   while (--index > 0) {
     for (let key in arguments[index]) {
-       if (key === "__proto__") {
-        continue;
+      if (key === '__proto__') {
+        continue
       }
-      arguments[index - 1][key] = arguments[index][key];
+      arguments[index - 1][key] = arguments[index][key]
     }
+  }
+  return arguments[0]
 }
- return arguments[0];
-}
+
+// 目的：将参数除了第一条数据的key都赋值更新到第一条数据内，实现assign合并功能
+// 1、第一步遍历行参，倒叙，不循环第一条
+// 2、for循环遍历行参，参数对象，做赋值处理（细节：key位__proto__原型参数时跳出循环体），行参[i-1]=行参[i] 实现向前更新
+// 3、将第一条数据return
