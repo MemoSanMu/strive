8f81f9fbd76bbf89603b85b4295e2fc69c98bff1
王森 <wangsen1@yonghui.cn>
2023-10-26 19:46:41 +0800

    chore(flb): 优化数组方式，

--- code/flb/index.js
+++ code/flb/index.js
@@ -14,6 +14,23 @@ const flb = (n) => {
   return arr[n];
 };
 
+// 优化数组方式，
+// 第一种方法可以在优化，因为不需要列出数组，只需要有前两值即可
+var fib = function (n) {
+  if (n <= 1) {
+    return n;
+  }
+  let dp1 = 0;
+  let dp2 = 1;
+  let dp3;
+  for (let i = 2; i <= n; i++) {
+    dp3 = dp1 + dp2;
+    dp1 = dp2;
+    dp2 = dp3;
+  }
+  return dp3;
+};
+
 // 递归方式
 const fbl2 = (n) => {
   if (n === 1 || n === 2) {
