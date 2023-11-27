3fd7e6f56c26a4dd2959d5592a68a586fd50a2c8
王森 <wangsen1@yonghui.cn>
2023-11-27 10:51:25 +0800

    chore(swiper): 完善完善

--- try/swiper/index.js
+++ try/swiper/index.js
@@ -1,8 +1,9 @@
 const doms = {
+  container: document.querySelector('.swiper-container'),
   list: document.querySelector('.swiper-list'),
   ind: document.querySelectorAll('.indicator span'),
   left: document.querySelector('.left'),
-  right: document.querySelector('.right'),
+  right: document.querySelector('.right')
 };
 
 let curIndex = 0;
@@ -26,6 +27,41 @@ function init() {
   });
 }
 init();
+
+let interval;
+
+const play = () => {
+  interval = setInterval(() => {
+    rightNext();
+  }, 2000);
+};
+
+play();
+
+doms.container.addEventListener('mouseover', () => {
+  clearInterval(interval);
+  interval = null;
+});
+doms.container.addEventListener('mouseout', () => {
+  play();
+});
+
+const handler = () => {
+  // 也可以通过document.hidden属性（布尔类型）来判断
+  // window.document.hidden (True/False)
+
+  // document.visibilityState拥有两种字符串枚举值（'visible' 和 'hidden'）
+  if (window.document.visibilityState === 'visible') {
+    // 当页签处于可见状态，TODO
+    play();
+  } else {
+    clearInterval(interval);
+    interval = null;
+    // 当页签处于不可见状态（hidden），TODO
+  }
+};
+
+window.document.addEventListener('visibilitychange', handler);
 
 function moveTo(index) {
   doms.list.style.transform = `translateX(-${index}00%)`;
