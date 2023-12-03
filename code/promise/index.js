a818708f32281c44a3e6c62cd70c8c910821ec94
wangsen <wangsen1@yonghui.cn>
2023-12-03 17:41:03 +0800

    chore(promise): 手写promise 2023-12-03

---
+++ code/promise/index.js
@@ -0,0 +1,242 @@
+const FULFILLED = 'fulfilled'; // 成功
+const REJECTED = 'rejected'; // 失败
+const PENDING = 'pending'; // 进行中
+
+function handlePromise(promise, x, resolve, reject) {
+  if (promise === x) {
+    return reject(
+      new TypeError('Chaining cycle detected for promise #<Promise>')
+    );
+  }
+  if (x && (typeof x === 'object' || typeof x === 'function')) {
+    let called;
+    try {
+      const then = x.then;
+      if (typeof then === 'function') {
+        then.call(
+          x,
+          (res) => {
+            if (called) return;
+            called = true;
+            handlePromise(promise, res, resolve, reject);
+          },
+          (err) => {
+            if (called) return;
+            called = true;
+            reject(err);
+          }
+        );
+      } else {
+        if (called) return;
+        called = true;
+        resolve(x);
+      }
+    } catch (error) {
+      if (called) return;
+      called = true;
+      reject(error);
+    }
+  } else {
+    resolve(x);
+  }
+}
+
+class MyPromise {
+  state = PENDING;
+  value = undefined;
+  reason = undefined;
+  resolvedCallbackList = [];
+  fulfilledCallbackList = [];
+  constructor(executor) {
+    function resolve(val) {
+      if (this.state === PENDING) {
+        this.state = FULFILLED;
+        this.value = val;
+        this.resolvedCallbackList.forEach((cb) => cb());
+      }
+    }
+    function reject(reason) {
+      if (this.state === PENDING) {
+        this.state = REJECTED;
+        this.reason = reason;
+        this.fulfilledCallbackList.forEach((cb) => cb());
+      }
+    }
+    try {
+      executor(resolve.bind(this), reject.bind(this));
+    } catch (error) {
+      reject(error);
+    }
+  }
+  then(resolve, reject) {
+    resolve = typeof resolve === 'function' ? resolve : (v) => v;
+    reject =
+      typeof reject === 'function'
+        ? reject
+        : (e) => {
+            throw e;
+          };
+    const resultPromise = new MyPromise((res, rej) => {
+      if (this.state === FULFILLED) {
+        setTimeout(() => {
+          try {
+            const r = resolve(this.value);
+            handlePromise(resultPromise, r, res, rej);
+          } catch (error) {
+            rej(error);
+          }
+        });
+      }
+      if (this.state === REJECTED) {
+        setTimeout(() => {
+          try {
+            const e = reject(this.reason);
+            handlePromise(resultPromise, e, res, rej);
+          } catch (error) {
+            rej(error);
+          }
+        });
+      }
+      if (this.state === PENDING) {
+        this.resolvedCallbackList.push(() => {
+          setTimeout(() => {
+            try {
+              const r = resolve(this.value);
+              handlePromise(resultPromise, r, res, rej);
+            } catch (error) {
+              rej(error);
+            }
+          });
+        });
+        this.fulfilledCallbackList.push(() => {
+          setTimeout(() => {
+            try {
+              const e = reject(this.reason);
+              handlePromise(resultPromise, e, res, rej);
+            } catch (error) {
+              rej(error);
+            }
+          });
+        });
+      }
+    });
+    return resultPromise;
+  }
+  catch() {
+    return this.then(null, arguments[0]);
+  }
+  // mdn 规范
+  // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Promise/finally
+  finally(onFinally) {
+    return this.then(
+      (r) => this.resolve(onFinally()).then(() => r),
+      (err) =>
+        this.resolve(onFinally()).then(() => {
+          throw err;
+        })
+    );
+  }
+  resolve(res) {
+    if (res instanceof Promise) {
+      return res;
+    }
+    if (res && typeof res === 'object' && typeof res.then === 'function') {
+      return new MyPromise((res, rej) => {
+        res.then(res, rej);
+      });
+    }
+    return new MyPromise((resolve) => {
+      resolve(res);
+    });
+  }
+  reject(reason) {
+    return new MyPromise((_, reject) => {
+      reject(reason);
+    });
+  }
+  all(promises) {
+    return new MyPromise((resolve, reject) => {
+      let count = 0;
+      let result = [];
+      for (let index = 0; index < promises.length; index++) {
+        MyPromise.resolve(promises[index]).then(
+          (res) => {
+            result.push(res);
+            count++;
+            if (count === promises.length) {
+              resolve(result);
+            }
+          },
+          (err) => {
+            reject(err);
+          }
+        );
+      }
+    });
+  }
+  race(promises) {
+    return new MyPromise((resolve, reject) => {
+      for (let index = 0; index < promises.length; index++) {
+        // 谁先执行完，谁就先执行
+        MyPromise.resolve(promises[index]).then(resolve, reject);
+      }
+    });
+  }
+}
+
+const p = new MyPromise((resolve, reject) => {
+  // reject(1);
+  setTimeout(() => {
+    resolve(1);
+  }, 2000);
+})
+  .then(
+    (v) => {
+      console.log(v, 'then');
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(2);
+        }, 1000);
+      });
+    },
+    (e) => {
+      console.log(e, 'catch');
+    }
+  )
+  .then(
+    (v) => {
+      console.log(v, 'then2');
+      return { 3: '3' };
+    },
+    (e) => {
+      console.log(e, 'catch');
+    }
+  )
+  .then(
+    (v) => {
+      console.log(v, 'then3');
+    },
+    (e) => {
+      console.log(e, 'catch');
+    }
+  );
+
+// 通过插件 promise-aplus-tests 跑测试用例
+// 第一步 全局下载 promises-aplus-tests  yarn global add promises-aplus-tests
+// 第二步 创建 package.json 文件,写入以下内容
+// {
+//   "scripts": {
+//     "test": "promises-aplus-tests ./js/promise.class.js"
+//   }
+// }
+// 第三步 执行 yarn test
+
+MyPromise.defer = MyPromise.deferred = function () {
+  let dfd = {};
+  dfd.promise = new MyPromise((resolve, reject) => {
+    dfd.resolve = resolve;
+    dfd.reject = reject;
+  });
+  return dfd;
+};
+module.exports = MyPromise;
--- js/promise.js
+++ js/promise.js
@@ -1,75 +1,75 @@
 // 首先 promise 这样用的吧
 // new Promise((resolve, reject) => {})
 
-const PENDING = 'pending'
-const FULFILLED = 'fulfilled'
-const REJECTED = 'rejected'
+const PENDING = 'pending';
+const FULFILLED = 'fulfilled';
+const REJECTED = 'rejected';
 
 // 基础用法
 function MyPromise(executor) {
-  this.state = PENDING
-  this.value = undefined
-  this.reason = undefined
-  this.onFulfilledCallback = [] // 存放成功时的回调函数
-  this.onRejectedCallback = [] // 存放时的失败的回调函数
+  this.state = PENDING;
+  this.value = undefined;
+  this.reason = undefined;
+  this.onFulfilledCallback = []; // 存放成功时的回调函数
+  this.onRejectedCallback = []; // 存放时的失败的回调函数
 
   // resolveFn（）和rejected（）这里为什么要用setTimeout将它变为异步执行呢？
   // 因为如果不用setTimeout这种方式的话，若Promise里面的代码是同步代码，在执行到reject或者resolve的时候，还没有执行then，所以数组里还没有值，
   // 这个时候调用的话不会报错但是不会输出任何结果，用setTimeout转为异步的话，会先去执行then方法，将回调收集到数组里，然后再去执行异步任务，这个时候就有值了
   const resolveFn = (res) => {
     if (this.state === PENDING) {
-      this.state = FULFILLED
-      this.value = res
+      this.state = FULFILLED;
+      this.value = res;
       // 异步完成后 将存储的then函数回调执行并且返回(透传)resolve的结果，在then的时候就能取到 promise内部resolve的 'res'
-      this.onFulfilledCallback.forEach((fn) => fn())
+      this.onFulfilledCallback.forEach((fn) => fn());
     }
-  }
+  };
 
   const rejectFn = (reason) => {
     if (this.state === PENDING) {
-      this.state = REJECTED
-      this.reason = reason
-      this.onRejectedCallback.forEach((fn) => fn())
+      this.state = REJECTED;
+      this.reason = reason;
+      this.onRejectedCallback.forEach((fn) => fn());
     }
-  }
+  };
   try {
-    executor(resolveFn, rejectFn)
+    executor(resolveFn, rejectFn);
   } catch (error) {
-    rejectFn(error)
+    rejectFn(error);
   }
 }
 
 // 执行then，将then的cb参数存储到对应的执行队列中
 MyPromise.prototype.then = function (resolve, reject) {
-  resolve = typeof resolve === 'function' ? resolve : (v) => v
+  resolve = typeof resolve === 'function' ? resolve : (v) => v;
   reject =
     typeof reject === 'function'
       ? reject
       : (e) => {
-          throw e
-        }
+          throw e;
+        };
   const resultNewPromise = new MyPromise((newResolve, newReject) => {
     // 已完成 状态 直接执行
     if (this.state === FULFILLED) {
       setTimeout(() => {
         try {
-          const res = resolve(this.value)
-          handleResolvePromise(resultNewPromise, res, newResolve, newReject)
+          const res = resolve(this.value);
+          handleResolvePromise(resultNewPromise, res, newResolve, newReject);
         } catch (error) {
-          newReject(error)
+          newReject(error);
         }
-      })
+      });
     }
     // 已失败 状态 直接执行
     if (this.state === REJECTED) {
       setTimeout(() => {
         try {
-          const res = reject(this.reason)
-          handleResolvePromise(resultNewPromise, res, newResolve, newReject)
+          const res = reject(this.reason);
+          handleResolvePromise(resultNewPromise, res, newResolve, newReject);
         } catch (error) {
-          newReject(error)
+          newReject(error);
         }
-      })
+      });
     }
     // 等待状态
     // 把then的回掉函数存储
@@ -77,123 +77,123 @@ MyPromise.prototype.then = function (resolve, reject) {
       this.onFulfilledCallback.push(() => {
         setTimeout(() => {
           try {
-            const res = resolve(this.value)
-            handleResolvePromise(resultNewPromise, res, newResolve, newReject)
+            const res = resolve(this.value);
+            handleResolvePromise(resultNewPromise, res, newResolve, newReject);
           } catch (error) {
-            newReject(error)
+            newReject(error);
           }
-        })
-      })
+        });
+      });
       this.onRejectedCallback.push(() => {
         setTimeout(() => {
           try {
-            const res = reject(this.reason)
-            handleResolvePromise(resultNewPromise, res, newResolve, newReject)
+            const res = reject(this.reason);
+            handleResolvePromise(resultNewPromise, res, newResolve, newReject);
           } catch (error) {
-            newReject(error)
+            newReject(error);
           }
-        })
-      })
+        });
+      });
     }
-  })
-  return resultNewPromise
-}
+  });
+  return resultNewPromise;
+};
 
 MyPromise.prototype.resolve = (v) => {
   if (v instanceof MyPromise) {
-    return v
+    return v;
   }
   if (typeof v === 'object') {
     return new MyPromise((resolve, reject) => {
       if (v.then) {
         if (typeof v.then === 'function') {
-          v.then(resolve, reject)
+          v.then(resolve, reject);
         } else {
-          resolve(v)
+          resolve(v);
         }
       } else {
-        resolve(v)
+        resolve(v);
       }
-    })
+    });
   } else {
     return new MyPromise((resolve) => {
-      resolve(v)
-    })
+      resolve(v);
+    });
   }
-}
+};
 
 MyPromise.prototype.all = (promises) => {
   return new MyPromise((resolve, reject) => {
-    let count = 0
-    const result = []
+    let count = 0;
+    const result = [];
     for (let index = 0; index < promises.length; index++) {
       MyPromise.prototype.resolve(promises[index]).then(
         (res) => {
-          result.push(res)
-          count++
+          result.push(res);
+          count++;
           if (count === promises.length) {
-            resolve(result)
+            resolve(result);
           }
         },
         (err) => reject(err)
-      )
+      );
     }
-  })
-}
+  });
+};
 
 MyPromise.prototype.race = (promises) => {
   return new MyPromise((resolve, reject) => {
     for (let index = 0; index < promises.length; index++) {
-      MyPromise.prototype.resolve(promises[index]).then(resolve, reject)
+      MyPromise.prototype.resolve(promises[index]).then(resolve, reject);
     }
-  })
-}
+  });
+};
 
 MyPromise.prototype.catch = (reject) => {
-  return MyPromise.prototype.then(undefined, reject)
-}
+  return MyPromise.prototype.then(undefined, reject);
+};
 
 MyPromise.prototype.finally = (fn) => {
-  return MyPromise.prototype.then(fn, fn)
-}
+  return MyPromise.prototype.then(fn, fn);
+};
 
 function handleResolvePromise(newP, result, resolve, reject) {
   // 循环引用
   if (newP === result) {
-    reject(new TypeError('Chaining cycle'))
+    reject(new TypeError('Chaining cycle'));
   }
 
   // 如果result不为空，且是函数或者对象
   if (result && (typeof result === 'object' || typeof result === 'function')) {
-    let called
+    let called;
     try {
-      const then = result.then
+      const then = result.then;
       if (typeof then === 'function') {
         then.call(
           result,
           (res) => {
-            if (called) return
-            called = true
-            handleResolvePromise(newP, res, resolve, reject)
+            if (called) return;
+            called = true;
+            handleResolvePromise(newP, res, resolve, reject);
           },
           (err) => {
-            if (called) return
-            called = true
-            reject(err)
+            if (called) return;
+            called = true;
+            reject(err);
           }
-        )
+        );
       } else {
-        if (called) return
-        called = true
-        resolve(result)
+        if (called) return;
+        called = true;
+        resolve(result);
       }
     } catch (error) {
-      if (called) return
-      called = true
-      reject(error)
+      if (called) return;
+      called = true;
+      reject(error);
     }
   } else {
-    resolve(result)
+    resolve(result);
   }
 }
 
@@ -202,42 +202,42 @@ const pro = new MyPromise((resolve, reject) => {
   // 一步调用接口
   setTimeout(() => {
     // 成功后返回
-    resolve('res')
-  }, 1000)
+    resolve('res');
+  }, 1000);
 
   // 接口异常 ｜ 接口异常
   // reject('error')
 })
   .then((res) => {
-    console.log(`第一次log ${res}`)
+    console.log(`第一次log ${res}`);
     return new MyPromise((resolve) => {
       setTimeout(() => {
-        resolve(`新new promise`)
-      }, 200)
-    })
+        resolve(`新new promise`);
+      }, 200);
+    });
   })
   .then()
   .then((res) => {
-    console.log(`第二次log ${res}`)
-    return `第二次return ${res}`
+    console.log(`第二次log ${res}`);
+    return `第二次return ${res}`;
   })
   .then((res) => {
-    console.log(`第三次log ${res}`)
+    console.log(`第三次log ${res}`);
     return {
       res,
-    }
+    };
   })
   .then((res) => {
-    console.log('last then', res)
-  })
+    console.log('last then', res);
+  });
 
 // 在手写的promiseXXX.js添加以下代码，其中改成自己定义promise.js名字
-// MyPromise.defer = MyPromise.deferred = function () {
-//   let dfd = {}
-//   dfd.promise = new MyPromise((resolve, reject) => {
-//     dfd.resolve = resolve
-//     dfd.reject = reject
-//   })
-//   return dfd
-// }
-// module.exports = MyPromise
+MyPromise.defer = MyPromise.deferred = function () {
+  let dfd = {};
+  dfd.promise = new MyPromise((resolve, reject) => {
+    dfd.resolve = resolve;
+    dfd.reject = reject;
+  });
+  return dfd;
+};
+module.exports = MyPromise;
