b09b3a13660540f0be61d1c9fc0a3d20102248b0
王森 <wangsen1@yonghui.cn>
2023-12-12 11:39:27 +0800

    chore(objectasign): objectASign

---
+++ code/object.asign/index.js
@@ -0,0 +1,34 @@
+// function objectASign() {
+//   const args = Array.from(arguments);
+//   let res = {};
+//   for (let index = 0; index < args.length; index++) {
+//     for (const key in args[index]) {
+//       if (key === '__proto__') {
+//         continue;
+//       }
+//       res[key] = args[index][key];
+//     }
+//   }
+//   return res;
+// }
+
+function objectASign() {
+  const args = Array.from(arguments);
+  for (let index = args.length - 1; index > 0; index--) {
+    for (const key in args[index]) {
+      if (key === '__proto__') {
+        continue;
+      }
+      args[index - 1][key] = args[index][key];
+    }
+  }
+  return args[0];
+}
+
+const obj1 = { a: 1, b: 2, c: 'cc' };
+const obj2 = { c: 3, d: 4 };
+const obj3 = { a: 3, d: 7, f: 4, g: 5, c: 'c' };
+
+const res = objectASign(obj1, obj2, obj3);
+
+console.log(res, 'res');
