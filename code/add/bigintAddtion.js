0b0d696fecd22b5c5e6066c9acd43602916cd5da
wangsen <wangsen1@yonghui.cn>
2023-12-16 16:25:10 +0800

    chore(addtion): 大数相加

---
+++ code/add/bigintAddtion.js
@@ -0,0 +1,25 @@
+let a = '111199999';
+let b = '99999';
+
+function bigintAddtion(a, b) {
+  const maxLen = Math.max(a.length, b.length);
+  a = a.padStart(maxLen, 0);
+  b = b.padStart(maxLen, 0);
+
+  let add = 0;
+  let res = '';
+
+  let i = maxLen - 1;
+  while (i >= 0) {
+    const val = Number(a[i]) + Number(b[i]) + add;
+    res = (val % 10) + res;
+    add = Math.floor(val / 10);
+    i--;
+  }
+  if (add) {
+    res = add + res;
+  }
+  return res;
+}
+
+console.log(bigintAddtion(a, b));
