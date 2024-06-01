6b039ff71ae7fb4a756b2035d36eb7093c6b3602
wangsen <wangsen1@yonghui.cn>
2024-06-01 16:24:12 +0800

    chore(2024-06): 版本号排序

--- 2024/versions.js
+++ 2024/versions.js
@@ -1,28 +1,38 @@
 // // [1.0.1, 1.1.0, 1.0.2, 2.0.0]
 
-const versions = ['1.0.1', '1.1.0', '1.0.2', '2.0.0', '3', '9', '5.6', '1.2', '1.2.4']
+const versions = [
+  '1.0.1',
+  '1.1.0',
+  '1.0.2',
+  '2.0.0',
+  '3',
+  '9',
+  '5.6',
+  '1.2',
+  '1.2.4',
+];
 
 function sortVersions(versions) {
   return versions.sort((a, b) => {
-    const left = a.split('.')
-    const right = b.split('.')
-    const max = Math.max(left.length, right.length)
-    let i = 0
+    const left = a.split('.');
+    const right = b.split('.');
+    const max = Math.max(left.length, right.length);
+    let i = 0;
     while (i < max) {
-      const l = left[i]
-      const r = right[i]
-      i++
+      const l = left[i];
+      const r = right[i];
+      i++;
       if (l === undefined || r === undefined) {
-        return left.length - right.length
+        return left.length - right.length;
       }
       if (l === r) {
-        continue
+        continue;
       }
-      return l - r
+      return l - r;
     }
-  })
+  });
 }
 
-const res = sortVersions(versions)
+const res = sortVersions(versions);
 
-console.log(res)
+console.log(res);
