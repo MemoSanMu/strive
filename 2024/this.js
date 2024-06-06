ffac6bbfb4556a7df8ce1aa71a1f21d6566c5404
wangsen <wangsen1@yonghui.cn>
2024-06-06 11:22:32 +0800

    chore(06-06): call 回顾

--- 2024/this.js
+++ 2024/this.js
@@ -1,37 +1,38 @@
 Function.prototype.myCall = function (context, ...args) {
   if (typeof this !== 'function') {
-    throw new TypeError('被调用必须是函数')
+    throw new TypeError('被调用必须是函数');
   }
-  content = context || globalThis
-  const self = symbol('fn')
-  context[self] = this
-  const res = context[self](...args)
-  delete context[self]
-  return res
-}
+  content = context === undefined || null ? globalThis : Object(context);
+
+  const self = symbol('fn');
+  context[self] = this;
+  const res = context[self](...args);
+  delete context[self];
+  return res;
+};
 
 Function.prototype.myBind = function (context, ...args) {
   if (typeof this !== 'function') {
-    throw new TypeError('被调用必须是函数')
+    throw new TypeError('被调用必须是函数');
   }
-  const self = this
+  const self = this;
   return function () {
-    const combineArgs = [...args, ...arguments]
+    const combineArgs = [...args, ...arguments];
     if (new.target) {
-      return new self(...combineArgs) // new 调用函数
+      return new self(...combineArgs); // new 调用函数
     }
-    return self.apply(context, combineArgs) // 执行调用函数
-  }
-}
+    return self.apply(context, combineArgs); // 执行调用函数
+  };
+};
 
 Function.prototype.myApply = function (context, args) {
   if (typeof this !== 'function') {
-    throw new TypeError('被调用必须是函数')
+    throw new TypeError('被调用必须是函数');
   }
-  content = context || globalThis
-  const self = symbol('fn')
-  context[self] = this
-  const res = context[self](...args)
-  delete context[self]
-  return res
-}
+  content = context === undefined || null ? globalThis : Object(context);
+  const self = symbol('fn');
+  context[self] = this;
+  const res = context[self](...args);
+  delete context[self];
+  return res;
+};
