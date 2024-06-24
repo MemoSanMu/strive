bcf7f9b08554181b245c583630b91732bf4a0a67
wangsen <wangsen1@yonghui.cn>
2024-06-24 19:42:03 +0800

    chore(06-24): 大数相加，今天面试遇到了。

--- code/add/bigintAdd.js
+++ code/add/bigintAdd.js
@@ -1,24 +1,24 @@
 function bigIntAdd(a, b) {
-  const maxLen = Math.max(String(a).length, String(b).length) // 获取最长的长度
+  const maxLen = Math.max(String(a).length, String(b).length); // 获取最长的长度
   // 向前补零
-  a = a.padStart(maxLen, 0)
-  b = b.padStart(maxLen, 0)
+  a = a.padStart(maxLen, 0);
+  b = b.padStart(maxLen, 0);
 
   let ans = '',
-    add = 0
+    add = 0;
   for (let i = maxLen - 1; i >= 0; i--) {
-    const val = Number(a[i]) + Number(b[i]) + add
-    ans = `${val % 10}${ans}`
-    add = Math.floor(val / 10) // 获取十位数，注意需要向下取整
+    const val = Number(a[i]) + Number(b[i]) + add;
+    ans = `${val % 10}${ans}`;
+    add = Math.floor(val / 10); // 获取十位数，注意需要向下取整
   }
   if (add) {
-    ans = `${add}${ans}`
+    ans = `${add}${ans}`;
   }
 
-  return ans
+  return ans;
 }
 
-let a = '99999'
-let b = '99999'
+let a = '1236543';
+let b = '12365445';
 
-console.log(bigIntAdd(a, b))
+console.log(bigIntAdd(a, b));
